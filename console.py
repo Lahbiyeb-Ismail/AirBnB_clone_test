b5d7567e2d405c7a7694010662097aa7791b22cf
Lahbiyeb-Ismail <ismaillhbibe@gmail.com>
2024-03-06 18:56:55 +0100

    Updating the FileStorage class  to manage correctly serialization and deserialization of all our new classes

--- console.py
+++ console.py
@@ -3,10 +3,23 @@
 import cmd
 
 from models import storage
+from models.amenity import Amenity
 from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
 from models.user import User
 
-classes = {"BaseModel": BaseModel, "User": User}
+classes = {
+    "BaseModel": BaseModel,
+    "User": User,
+    "Place": Place,
+    "City": City,
+    "State": State,
+    "Amenity": Amenity,
+    "Review": Review,
+}
 
 
 class HBNBCommand(cmd.Cmd):
