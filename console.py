4dc1adc5131f0cb884575c21db2d80d78ad4941f
Lahbiyeb-Ismail <ismaillhbibe@gmail.com>
2024-03-06 15:57:05 +0100

    adding the 'show' command to the console script

--- console.py
+++ console.py
@@ -2,6 +2,7 @@
 
 import cmd
 
+from models import storage
 from models.base_model import BaseModel
 
 
@@ -33,6 +34,28 @@ class HBNBCommand(cmd.Cmd):
         new_model = BaseModel()
         new_model.save()
         print(new_model.id)
+
+    def do_show(self, arg):
+        """Prints the string representation of an instance"""
+        args = arg.split()
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        if args[0] != "BaseModel":
+            print("** class doesn't exist **")
+            return
+        if len(args) == 1:
+            print("** instance id missing **")
+            return
+
+        obj_dict = storage.all()
+        key = args[0] + "." + args[1]
+
+        if key not in obj_dict:
+            print("** no instance found **")
+            return
+
+        print(BaseModel.__str__(BaseModel(**obj_dict[key])))
 
 
 if __name__ == "__main__":
