79b06477f37eaeca9641a9617f57ffa3cf4e2fb3
Lahbiyeb-Ismail <ismaillhbibe@gmail.com>
2024-03-09 16:03:18 +0100

    updating the FileStorage class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -14,21 +14,11 @@ class FileStorage:
         FileStorage.__object[key] = obj.to_dict()
 
     def save(self):
-        """Serializes __objects to the JSON file (path: __file_path)"""
-        obj_dict = {
-            obj_id: obj.to_dict() for obj_id, obj in FileStorage.__objects.items()
-        }
-        with open(FileStorage.__file_path, "w") as f:
-            json.dump(obj_dict, f)
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
+            json.dump(FileStorage.__object, f, indent=4)
 
     def reload(self):
-        """Deserializes the JSON file to __objects"""
-        try:
-            with open(FileStorage.__file_path, "r") as f:
-                objs = json.load(f)
-                for obj_id, obj_dict in objs.items():
-                    cls_name = obj_dict["__class__"]
-                    cls = globals()[cls_name]
-                    FileStorage.__objects[obj_id] = cls(**obj_dict)
-        except FileNotFoundError:
+        with open(FileStorage.__file_path, "a+", encoding="utf-8") as f:
             pass
+        with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
+            FileStorage.__object = json.load(f)
